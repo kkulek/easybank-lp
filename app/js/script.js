1876ddee0bb3dedce2de2c4c11d9652df6d42ddf
kkulek <krzysztof.kulka@hotmail.com>
2022-09-08 16:17:12 +0200

    responsive LP

---
+++ app/js/script.js
@@ -0,0 +1,26 @@
+const btnHamburger = document.querySelector('#buttonHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() { //close Hamburger menu
+    if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
+    }
+    else { //open Hamburger menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+
+    }
+})
